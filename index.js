627002d641e1c2b0257914805b5ba3cd651d554e
Chr0lo <163361631+Chr0lo@users.noreply.github.com>
2024-08-19 06:27:45 +0300

    goBeta

--- index.js
+++ index.js
@@ -51,7 +51,22 @@ bot.onText(/\/start/, (msg) => {
     const userName = msg.from.first_name; // Numele utilizatorului
 
     // Textul mesajului și butonul "Play"
-    const text = `Bun venit, ${userName}! Apasă pe butonul de mai jos pentru a începe:`;
+    const text = `Welcome, ${userName}! 🎮 *Welcome to the Beta version of Tektoniks!* 🎮
+
+We're excited to have you among the first players testing this limited version. 
+During the beta period, a select group of people, including you, will have exclusive access to the game's features.
+
+🔄 *What you need to know about the beta:*
+
+- The game is still in development, so some features are currently unavailable.
+*The features will be added gradually to ensure that each one is thoroughly tested and works correctly. This process 
+helps us identify and resolve any issues before introducing the next set of features.
+- We will be adding new features gradually, every 2-3 days. These include the shop, the earn system, and more.
+- We greatly appreciate your feedback! If you encounter any bugs or have suggestions, please don't hesitate to let us know.
+
+🚀 *What's next:*
+
+Stay tuned for upcoming updates and explore new features as soon as they become available. Thank you for helping us make this game better!`;
     const options = {
         reply_markup: {
             inline_keyboard: [
